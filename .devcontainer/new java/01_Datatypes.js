1bc1915445e0375f256c4a5c3f04f0efb18b8949
Pushkar Deore <deorepushkar2305@gmail.com>
2024-07-16 07:30:33 +0000

    datatypes of js

---
+++ .devcontainer/new java/01_Datatypes.js
@@ -0,0 +1,12 @@
+//datatypes
+//numbers
+//bigint
+//boolean==>true/false
+//string=>""
+//null=.standalone value also called as object 
+//undefined=>only variable declare not value assigned
+//symbol 
+//object
+console.log(typeof "pushkar");//output as string
+console.log(typeof null);//output as object
+
